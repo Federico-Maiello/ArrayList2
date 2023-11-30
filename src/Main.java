8c5023e48ae10fcaff72eb076ae994ab450734c0
Federico Maiello <federicomaiello@hotmail.it>
2023-11-30 11:35:40 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Student student = new Student("Federico Maiello", 28);
         List<Student> lista1 = new ArrayList<>();
-//        lista1.add(student.name);
-//        lista1.add(String.valueOf(student.age));
-//        lista1.add("H 1.85");
-//        lista1.add("Occhi marroni");
-//        lista1.add("Capelli neri");
-//        lista1.add("Colore preferito NERO");
-//        lista1.add("Hobby Pianoforte");
-//        lista1.add("Casata: Tasso rosso");
-//        lista1.add("Film Harry Potter");
-//        lista1.add("Istruzione: Diploma");
-//        lista1.add("Futuro lavoro: Pensionato");
-//        lista1.add("Id: 774");
         lista1.add(new Student("Giancarlo", 24));
         lista1.add(new Student("Francesco", 28));
         lista1.add(new Student("Marta", 26));
